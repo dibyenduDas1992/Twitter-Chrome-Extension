26d42af14b19f2b25e6f3309558ac30974eb5d02
Dibyendu Das <das.dibyendu1992@gmail.com>
2016-06-21 03:02:00 +0530

    Final commit 2

--- background.js
+++ background.js
@@ -5,7 +5,6 @@ for (var eachContext in contextList) {
         title: title,
         contexts: [contextList[eachContext]],
         onclick: function(object, tab) {
-            alert(JSON.stringify(object));
             switch (object.menuItemId) {
                 case 'selection':
                     chrome.windows.create({
